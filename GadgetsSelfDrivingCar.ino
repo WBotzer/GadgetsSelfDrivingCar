9ef99183a3542d9a9401f478bcfdd37d41fe1786
byankello18 <guitary18@gmail.com>
2015-11-24 15:41:29 -0500

    Finite State Machine for driving

--- GadgetsSelfDrivingCar.ino
+++ GadgetsSelfDrivingCar.ino
@@ -11,8 +11,25 @@
 //Rear motor can be operated at other speeds sparingly as motor failure can occur
 #define SPEED_2 170 //Approx. 6V to motors
 #define TURBO 255 //Approx. 9V to motors
-#define TURN_THRESHOLD 90 //
- 
+#define TURN_THRESHOLD 95 //
+#define TURN_MINIMUM 40 //
+#define PING_ITERATIONS 5
+
+#define Forward 0
+#define Turn 1
+#define Turn_left 2
+#define Turn_right 3
+#define Reverse 4
+#define Reverse_right 5
+#define Reverse_left 6
+#define Backward 7
+
+ int state = 0;
+ bool MOVEMENT = false;
+ int left_dist;
+ int previous_front;
+ int front_dist;
+ int right_dist;
 
 // --------------------------------------------------------------------------- Motors
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
@@ -21,8 +38,8 @@ Adafruit_DCMotor *rearMotor = AFMS.getMotor(2);
 
 // --------------------------------------------------------------------------- Sensors
 NewPing front(5,4,MAX_DISTANCE);
-NewPing left(13,12,MAX_DISTANCE);
-NewPing right(9,8,MAX_DISTANCE);
+NewPing right(13,12,MAX_DISTANCE);
+NewPing left(9,8,MAX_DISTANCE);
 
 // --------------------------------------------------------------------------- Setup
 void setup() {
@@ -35,27 +52,98 @@ void setup() {
 
 // --------------------------------------------------------------------------- Loop
 void loop() { 
-	//Debugging Code
-  //int front_dist = front.ping_cm();
-//  int left_dist = left.ping_cm();
-  int right_dist = right.ping_cm();
+  left_dist = left.convert_cm(left.ping_median(PING_ITERATIONS));
+  if (left_dist == 0)
+    left_dist = 999;
+
+  previous_front = front_dist;
+  //front_dist = front.convert_cm(front.ping_median(PING_ITERATIONS));
+  front_dist = front.ping_cm();
+  if (front_dist == 0)
+    front_dist = 999;
+
+  if (previous_front != front_dist)
+    MOVEMENT = true;
+  else
+    MOVEMENT = false;
+    
+  right_dist = right.convert_cm(right.ping_median(PING_ITERATIONS));
+  if (right_dist == 0)
+    right_dist = 999;
   
-//  Serial.print(left_dist);
-  Serial.print(" ");
-//  Serial.print(front_dist);
-  Serial.print(" ");
-  Serial.print(right_dist);
-  Serial.print("\n");
-//  if( front_dist == 0 || front_dist < TURN_THRESHOLD) {
-//    turn_center();
-//    drive_forward();
-//  }
-//  else if(left_dist > right_dist){
-//    turn_left();
-//  }
-//  else {
-//    turn_right();
-//  }
+  switch (state) {
+    case Forward:
+      turn_center();
+      drive_forward();
+      if(front_dist < TURN_THRESHOLD && front_dist > TURN_MINIMUM)
+        state = Turn;
+      else if(front_dist < TURN_MINIMUM)
+        state = Reverse;
+      break;
+      
+    case Turn:
+      if(left_dist > right_dist)
+        state = Turn_left;
+      else
+        state = Turn_right;
+      break;
+      
+    case Turn_left:
+      turn_left();
+      if(front_dist > TURN_THRESHOLD)
+        state = Forward;
+      else if (front_dist < TURN_MINIMUM)
+        state = Reverse_right;
+      break;
+      
+    case Turn_right:
+      turn_right();
+      if(front_dist > TURN_THRESHOLD)
+        state = Forward;
+      else if (front_dist < TURN_MINIMUM)
+        state = Reverse_left;
+      break;
+      
+    case Reverse:
+      if(left_dist > right_dist)
+        state = Reverse_right;
+      else
+        state = Reverse_left;
+      break;
+      
+    case Reverse_right:
+      turn_left();
+      drive_backward();
+      if (front_dist > TURN_THRESHOLD || !MOVEMENT)
+        state = Forward;        
+      break;
+      
+    case Reverse_left:
+      turn_right();
+      drive_backward();
+      if (front_dist > TURN_THRESHOLD || !MOVEMENT)
+        state = Forward;  
+      break;
+      
+    case Backward:
+      break;
+  }
+
+  /*if(front_dist > TURN_THRESHOLD) {
+    turn_center();
+    drive_forward();
+  }
+  else if(front_dist > TURN_MINIMUM) {  // We have room to turn
+    if(left_dist > right_dist){
+      turn_left();
+    }
+    else {
+      turn_right();
+    }
+  }
+  else {  // We don't have room to complete a turn
+    if()
+  }*/
   
 	//!Debugging Code
 }
