a3af40d45e221df80bdbf3b80cdabe9171a59fe1
mobotus <mobotus@gmail.com>
2015-11-24 20:30:42 -0500

    Cleaned Code
    Defined variable block
    Cleaned logic

--- GadgetsSelfDrivingCar.ino
+++ GadgetsSelfDrivingCar.ino
@@ -3,7 +3,6 @@
 #include <Wire.h>
 #include "utility/Adafruit_PWMServoDriver.h"
 #include <NewPing.h>
-//#include <PrintEx.h>
 
 // --------------------------------------------------------------------------- Constants
 #define MAX_DISTANCE 300 //centimeters range 2 - 400
@@ -12,10 +11,10 @@
 //Rear motor can be operated at other speeds sparingly as motor failure can occur
 #define SPEED_2 170 //Approx. 6V to motors
 #define TURBO 255 //Approx. 9V to motors
-#define TURN_THRESHOLD 95 //
-#define TURN_MINIMUM 40 //
-#define PING_ITERATIONS 5
-
+#define TURN_THRESHOLD 95 //In cm
+#define TURN_MINIMUM 40 //in cm
+#define PING_ITERATIONS 5 //Count
+//For switch cases in loop()
 #define Forward 0
 #define Turn 1
 #define Turn_left 2
@@ -25,12 +24,13 @@
 #define Reverse_left 6
 #define Backward 7
 
- int state = 0;
- bool MOVEMENT = false;
- int left_dist;
- int previous_front;
- int front_dist;
- int right_dist;
+// --------------------------------------------------------------------------- Variables
+int state = Forward;
+bool MOVEMENT = false;
+int left_dist;
+int previous_front = 0;
+int front_dist;
+int right_dist;	
 
 // --------------------------------------------------------------------------- Motors
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
@@ -43,7 +43,6 @@ NewPing right(13,12,MAX_DISTANCE);
 NewPing left(9,8,MAX_DISTANCE);
 
 // --------------------------------------------------------------------------- Setup
-//StreamEx mySerial = Serial;//Debugging
 
 void setup() {
 	// Setup motors
@@ -54,105 +53,91 @@ void setup() {
 }
 
 // --------------------------------------------------------------------------- Loop
-void loop() { 
-  left_dist = left.convert_cm(left.ping_median(PING_ITERATIONS));
-  if (left_dist == 0)
-    left_dist = 999;
+void loop() {
 
-  previous_front = front_dist;
-  //front_dist = front.convert_cm(front.ping_median(PING_ITERATIONS));
-  front_dist = front.ping_cm();
-  if (front_dist == 0)
-    front_dist = 999;
+	left_dist = left.convert_cm(left.ping_median(PING_ITERATIONS));
+	if (left_dist == 0)
+		left_dist = 999;
+	
+	right_dist = right.convert_cm(right.ping_median(PING_ITERATIONS));
+	if (right_dist == 0)
+		right_dist = 999;
 
-  if (previous_front != front_dist)
-    MOVEMENT = true;
-  else
-    MOVEMENT = false;
+	front_dist = front.convert_cm(front.ping_median(PING_ITERATIONS));
+	if (front_dist == 0)
+		front_dist = 999;
+
+  
+	if (previous_front != front_dist)
+		MOVEMENT = true;
+	else
+		MOVEMENT = false;
     
-  right_dist = right.convert_cm(right.ping_median(PING_ITERATIONS));
-  if (right_dist == 0)
-    right_dist = 999;
+	previous_front = front_dist;
   
-  switch (state) {
-    case Forward:
-      turn_center();
-      drive_forward();
-      if(front_dist < TURN_THRESHOLD && front_dist > TURN_MINIMUM)
-        state = Turn;
-      else if(front_dist < TURN_MINIMUM)
-        state = Reverse;
-      break;
+	switch (state) {
+		case Forward:
+			turn_center();
+			drive_forward();
+			if(front_dist < TURN_THRESHOLD && front_dist > TURN_MINIMUM)
+				state = Turn;
+			else if(front_dist < TURN_MINIMUM)
+				state = Reverse;
+			break;
       
-    case Turn:
-      if(left_dist > right_dist)
-        state = Turn_left;
-      else
-        state = Turn_right;
-      break;
+		case Turn:
+			if(left_dist > right_dist)
+				state = Turn_left;
+			else
+				state = Turn_right;
+			break;
       
-    case Turn_left:
-      turn_left();
-      if(front_dist > TURN_THRESHOLD)
-        state = Forward;
-      else if (front_dist < TURN_MINIMUM)
-        state = Reverse_right;
-      break;
+		case Turn_left:
+			turn_left();
+			if(front_dist > TURN_THRESHOLD)
+				state = Forward;
+			else if (front_dist < TURN_MINIMUM)
+				state = Reverse_right;
+			break;
       
-    case Turn_right:
-      turn_right();
-      if(front_dist > TURN_THRESHOLD)
-        state = Forward;
-      else if (front_dist < TURN_MINIMUM)
-        state = Reverse_left;
-      break;
+		case Turn_right:
+			turn_right();
+			if(front_dist > TURN_THRESHOLD)
+				state = Forward;
+			else if (front_dist < TURN_MINIMUM)
+				state = Reverse_left;
+			break;
       
-    case Reverse:
-      if(left_dist > right_dist)
-        state = Reverse_right;
-      else
-        state = Reverse_left;
-      break;
+		case Reverse:
+			if(left_dist > right_dist)
+				state = Reverse_right;
+			else
+				state = Reverse_left;
+			break;
       
-    case Reverse_right:
-      turn_left();
-      drive_backward();
-      if (front_dist > TURN_THRESHOLD || !MOVEMENT)
-        state = Forward;        
-      break;
+		case Reverse_right:
+			turn_left();
+			drive_backward();
+			if (front_dist > TURN_THRESHOLD || !MOVEMENT)
+				state = Forward;        
+			break;
       
-    case Reverse_left:
-      turn_right();
-      drive_backward();
-      if (front_dist > TURN_THRESHOLD || !MOVEMENT)
-        state = Forward;  
-      break;
+		case Reverse_left:
+			turn_right();
+			drive_backward();
+			if (front_dist > TURN_THRESHOLD || !MOVEMENT)
+				state = Forward;  
+			break;
+			
+		case Reverse:
+			break;
       
-    case Backward:
-      break;
-  }
-
-  /*if(front_dist > TURN_THRESHOLD) {
-    turn_center();
-    drive_forward();
-  }
-  else if(front_dist > TURN_MINIMUM) {  // We have room to turn
-    if(left_dist > right_dist){
-      turn_left();
-    }
-    else {
-      turn_right();
-    }
-  }
-  else {  // We don't have room to complete a turn
-    if()
-  }*/
-  
-	//!Debugging Code
+		default:
+			motor_stop();
+			turn_center();
+	}
 }
 
-// --------------------------------------------------------------------------- Detect
-// TBD
 //Methods for NewPing objects
 //sonar.ping(); - Send a ping, returns the echo time in microseconds or 0 (zero) if no ping echo within set distance limit
 //sonar.ping_in(); - Send a ping, returns the distance in inches or 0 (zero) if no ping echo within set distance limit
